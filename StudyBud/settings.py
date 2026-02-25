4c1eee2707b903cc075ea279e2085d42e7e34bda
soumiblink <soumiblink@gmail.com>
2026-02-25 22:22:24 +0530

    update

--- StudyBud/settings.py
+++ StudyBud/settings.py
@@ -138,3 +138,6 @@ MEDIA_ROOT = BASE_DIR/'static/images'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'base.User'
+
--- base/forms.py
+++ base/forms.py
@@ -1,6 +1,16 @@
 from django.forms import ModelForm
 from django.contrib.auth.forms import UserCreationForm
 from .models import Room , User
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['name', 'username', 'email', 'password1', 'password2']
+
 
 class RoomForm(ModelForm):
     class Meta:
